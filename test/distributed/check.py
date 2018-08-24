15dfc94febd3c519004462c1b56a6a9c9dc88cfe
Jimmy Charnley Kromann <jimmy@charnley.dk>
2018-08-24 14:01:56 +0200

    pretty colors

--- test/distributed/check.py
+++ test/distributed/check.py
@@ -2,15 +2,33 @@
 import numpy as np
 import sys
 
+
+CGREEN = '\033[32m'
+CRED = '\033[91m'
+CEND = '\033[0m'
+
+def passed(msg):
+    print(CGREEN + msg + CEND)
+    return
+
+def warning(msg):
+    print(CRED + msg + CEND)
+    return
+
+def error(msg):
+    print(CRED + msg + CEND)
+    return
+
+
 if len(sys.argv) != 3:
 	print ('Usage: %s alphas1 alphas2' % sys.argv[0])
 	exit(1)
 
 try:
     if np.allclose(*map(np.loadtxt, sys.argv[1:])):
-        print ('Ok.')
+        passed ('PASSED')
     else:
-        print ('Failed value error')
+        error ('Failed value error')
 except ValueError:
-    print("Failed, Shape error")
+    error ("Failed, Shape error")
 
