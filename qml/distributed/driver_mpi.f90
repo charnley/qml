0645a0647279ed5b85256f386e893b9965af6dfe
von Rudorff, Guido Falk <guido@vonrudorff.de>
2019-04-10 10:59:06 +0200

    Enable non-square data, closes #1

--- qml/distributed/driver_mpi.f90
+++ qml/distributed/driver_mpi.f90
@@ -20,27 +20,28 @@
 ! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ! SOFTWARE.
 
-
-! prototype external calculator of the kernel matrix and solver for alpha coefficients
-! makes use of a PBLAS implementation, here the MKL variant
-! core idea is to distribute the kernel matrix across MPI ranks to allow out-of-core datasets to be processed
-! 
-! CSCS: 
-!   load modules PrgEnv-cray/6.0.4 cray-mpich/7.6.0 daint-gpu intel/17.0.4.196  
-!   build with ifort driver_mpi.f90 -o driver_mpi  -L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_scalapack_lp64 -lmkl_gf_lp64 -lmkl_sequential -lmkl_core -lmkl_blacs_openmpi_lp64 -lpthread -lm -ldl  -m64 -I${MKLROOT}/include -Og -g -Wall -Wextra -pedantic -fimplicit-none -fcheck=all -fbacktrace
 program qml_driver
     use mpi
     implicit none
 
-    double precision, allocatable, dimension(:,:) :: Q
-    double precision, allocatable, dimension(:):: Y
+    ! representations (nmol, rep_size)
+    double precision, allocatable, dimension(:,:) :: reps_i, reps_j
+
+    ! properties to learn
+    double precision, allocatable, dimension(:,:) :: Y
     
+    ! problem parameters 
+    integer :: n_i, n_j
+
+    ! kernel parameters
     double precision :: sigma
     integer :: rep_size
-    integer :: n_molecules
 
-    double precision, allocatable, dimension(:) :: alphas
+    ! model coefficients
+    double precision, allocatable, dimension(:, :) :: alphas
+    integer :: n_properties
 
+    ! pblacs data structures
     integer :: info
     integer :: na, i, numroc
     integer :: local_i, local_j, global_i, global_j
@@ -49,7 +50,6 @@ program qml_driver
     double precision, dimension(:), allocatable :: work
     double precision, dimension(:,:), allocatable :: local_K, local_B
 
-    ! pblacs data structures
     integer :: local_K_rows, local_K_cols
     integer :: local_B_rows, local_B_cols
     integer :: local_id, num_ranks, ranks_rows, ranks_cols, context
@@ -75,42 +75,45 @@ program qml_driver
     call blacs_gridinfo(context, ranks_rows, ranks_cols, &
         local_rank_row, local_rank_col)
 
+    !TODO : read on rank 0 only
     ! Read hyperparameters and arrat suzes
     open(unit = 9, file = "parameters.fout", form="formatted")
 
-        read(9,*) sigma, rep_size, n_molecules
+        read(9,*) sigma, rep_size, n_i, n_j
 
         ! Allocate labels
-        allocate(Y(n_molecules))
+        allocate(Y(n_i, n_properties))
 
-        read(9,*) Y(:n_molecules)
+        read(9,*) Y(:, n_i*n_properties)
 
     close(9)
     
     ! allocate alphas
-    allocate(alphas(n_molecules))
+    allocate(alphas(n_i, n_properties))
     ! Allocate representations
-    allocate(Q(n_molecules, rep_size))
+    allocate(reps_i(n_i, rep_size))
+    allocate(reps_j(n_j, rep_size))
 
     ! Read representations
-    open(unit = 9, file = "representations.fout", form="formatted")
-        
+    open(unit = 9, file = "representations_i.fout", form="formatted")
         ! Read representaions for each molecule
-        do i = 1, n_molecules 
-            read(9,*) Q(i,:rep_size)
+        do i = 1, n_i
+            read(9,*) reps_i(i,:rep_size)
         enddo
-
+    close(9)
+    open(unit = 9, file = "representations_j.fout", form="formatted")
+        ! Read representaions for each molecule
+        do i = 1, n_j
+            read(9,*) reps_j(i,:rep_size)
+        enddo
     close(9)
 
-    ! Size of kernel
-    na = size(Q, dim=1)
-
     ! Allocate kernel and output
-    local_K_rows = numroc(na, block_size, local_rank_row, 0, ranks_rows)
-    local_K_cols = numroc(na, block_size, local_rank_col, 0, ranks_cols)
+    local_K_rows = numroc(n_i, block_size, local_rank_row, 0, ranks_rows)
+    local_K_cols = numroc(n_j, block_size, local_rank_col, 0, ranks_cols)
     allocate(local_K(local_K_rows, local_K_cols))
-    local_B_rows = numroc(na, block_size, local_rank_row, 0, ranks_rows)
-    local_B_cols = numroc(1, block_size, local_rank_col, 0, ranks_cols)
+    local_B_rows = numroc(n_i, block_size, local_rank_row, 0, ranks_rows)
+    local_B_cols = numroc(n_properties, block_size, local_rank_col, 0, ranks_cols)
     allocate(local_B(local_B_rows, local_B_cols))
 
     ! Calculate Laplacian kernel
@@ -120,45 +123,46 @@ program qml_driver
         do local_i = 1, local_K_rows
             call l2g(local_i, local_rank_row, ranks_rows, &
                 block_size, global_i)
-            local_K(local_i, local_j) = exp(-sum(abs(Q(global_j,:) - Q(global_i,:)))/sigma)
+            local_K(local_i, local_j) = exp(-sum(abs(reps_j(global_j,:) - reps_i(global_i,:)))/sigma)
         enddo
     enddo
 
     ! Setup variables for LAPACK
-    call descinit(desca, na, na, block_size, block_size, 0, 0, context, MAX(1, local_K_rows), info)
-    call descinit(descb, na, 1, block_size, block_size, 0, 0, context, MAX(1, local_B_rows), info)
+    call descinit(desca, n_i, n_j, block_size, block_size, 0, 0, context, MAX(1, local_K_rows), info)
+    call descinit(descb, n_i, n_properties, block_size, block_size, 0, 0, context, MAX(1, local_B_rows), info)
 
     ! Allocate local work arrays
-    call pdgels("N", na, na, 1, local_K, 1, 1, desca, local_B, 1, 1, DESCB, work_query, -1, info)
+    call pdgels("N", n_i, n_j, n_properties, local_K, 1, 1, desca, local_B, 1, 1, DESCB, work_query, -1, info)
     lwork = INT(work_query(1))
     allocate(work(lwork))
 
     ! copy data
     local_B = 0.0d0
-    if (local_B_cols .ne. 0) then
+    do local_j = 1, local_B_cols
         do local_i = 1, local_B_rows
             call l2g(local_i, local_rank_row, ranks_rows, block_size, global_i)
-            local_B(local_i, 1) = y(global_i)
+            local_B(local_i, local_j) = Y(global_i, local_j)
         enddo
-    end if
+    enddo
 
     ! Solver
-    call pdgels("N", na, na, 1, local_K, 1, 1, desca, local_B, 1, 1, DESCB, work, lwork, info)
+    call pdgels("N", n_i, n_j, n_properties, local_K, 1, 1, desca, local_B, 1, 1, DESCB, work, lwork, info)
 
     ! Copy LAPACK output
     alphas = 0.0d0
-    if (local_B_cols .ne. 0) then
+    do local_j = 1, local_B_cols
         do local_i = 1, local_B_rows
             call l2g(local_i, local_rank_row, ranks_rows, block_size, global_i)
-            alphas(global_i) = local_B(local_i, 1)
+            call l2g(local_j, local_rank_col, ranks_cols, block_size, global_j)
+            alphas(global_i, global_j) = local_B(local_i, local_j)
         enddo
-    endif
+    enddo
     call DGSUM2D(context, "All", "1-tree", na, 1, alphas, 1, -1, -1)
 
     ! Save alphas to file
     if (local_id .eq. 0) then
         open(unit = 9, file = "alphas_mpi.fout", form="formatted")
-            write(9,*) alphas(:)
+            write(9,*) alphas(:,:)
         close(9)
     end if
    
@@ -168,7 +172,8 @@ program qml_driver
     ! Clean up 
     deallocate(work)
     deallocate(local_B)
-    deallocate(Q)
+    deallocate(reps_i)
+    deallocate(reps_j)
     deallocate(local_K)
     deallocate(Y)    
     deallocate(alphas)    
