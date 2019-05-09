ab59097efb663b1a0dce353f6575269e12823ae6
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-05-09 13:41:03 +0200

    fortran notation

--- test/distributed/check.py
+++ test/distributed/check.py
@@ -2,10 +2,15 @@
 import numpy as np
 import sys
 
-
 CGREEN = '\033[32m'
 CRED = '\033[91m'
 CEND = '\033[0m'
+
+def load(txtfile):
+    """ Read fortran scientific notation """
+    converters = {0: lambda s: float(s.replace('D', 'E') or float('nan'))}
+    arr = np.loadtxt(txtfile, converters=converters)
+    return arr
 
 def passed(msg):
     print(CGREEN + msg + CEND)
@@ -19,16 +24,26 @@ def error(msg):
     print(CRED + msg + CEND)
     return
 
-
 if len(sys.argv) != 3:
-	print ('Usage: %s alphas1 alphas2' % sys.argv[0])
-	exit(1)
+    print ('Usage: %s alphas1 alphas2' % sys.argv[0])
+    exit(1)
+
+args = sys.argv[1:]
 
 try:
-    if np.allclose(*map(np.loadtxt, sys.argv[1:])):
+
+    txt1 = args[0]
+    txt2 = args[0]
+
+    arr1 = load(txt1)
+    arr2 = load(txt2)
+
+    if np.allclose(arr1, arr2):
         passed ('PASSED')
+
     else:
-        error ('Failed value error')
+        error ('FAILED, Value error')
+
 except ValueError:
-    error ("Failed, Shape error")
+    error ("FAILED, Shape error")
 
